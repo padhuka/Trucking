754dbdf0180254b3e7f60ebaf354c8f9363804d2
dapras <dapras.solo@gmail.com>
2019-02-09 19:17:32 +0700

    FE-edit order

--- truck/pages/order/order_tab.php
+++ truck/pages/order/order_tab.php
@@ -227,6 +227,8 @@
 
             function batalubah(){
               refresh();
+              $('#listorder').show();
+              $('#addorder').hide();
             }
             function ubahorder(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q){       
 
